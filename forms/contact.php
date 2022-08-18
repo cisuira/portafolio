2d5ae1011ab63a3ba438fbed6ead118c32cc2168
cisuira <110005320+cisuira@users.noreply.github.com>
2022-08-18 11:50:26 -0500

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'suira-0104@hotmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -19,9 +19,9 @@
   $contact->ajax = true;
   
   $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  $contact->from_name = 'carlos';
+  $contact->from_email ='prueba@gmail.com';
+  $contact->subject = 'asunto de emails';
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -33,9 +33,9 @@
   );
   */
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  $contact->add_message( 'Carlos', 'From');
+  $contact->add_message( 'prueba@gmail.com', 'Email');
+  $contact->add_message( 'panama', 'Message', 10);
 
   echo $contact->send();
 ?>
